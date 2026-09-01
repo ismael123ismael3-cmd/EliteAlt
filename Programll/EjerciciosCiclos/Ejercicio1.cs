16be6c7f7e1daffa2106e2c90a9c0113b6d03125
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-01 15:09:42 -0400

    Ismael

---
+++ Programll/EjerciciosCiclos/Ejercicio1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programll.EjerciciosCiclos
+{
+    internal class Ejercicio1
+    {
+    }
+}
---
+++ Programll/EjerciciosCiclos/Ejercicio2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programll.EjerciciosCiclos
+{
+    public class Ejercicio_2
+    {
+        public static voidx
+    }
+}
