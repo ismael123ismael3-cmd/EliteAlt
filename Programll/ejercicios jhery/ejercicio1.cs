b6fbaf14c54e20a305954ab762c5cce9030a9b65
jhery vasquez <jhery1005@gmail.com>
2026-09-01 15:16:44 -0400

    Jhery

---
+++ Programll/ejercicios jhery/ejercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programll.ejercicios_jhery
+{
+    public class ejercicio1
+    {
+        public static void Main;
+      {
+            Console.WriteLine("--- EJERCICIO 1: Tabla de Multiplicar ---");
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 12; i++)
+        {
+            Console.WriteLine(numero + " x " + i + " = " + (numero* i));
+        }
+}
+
+}
+}
