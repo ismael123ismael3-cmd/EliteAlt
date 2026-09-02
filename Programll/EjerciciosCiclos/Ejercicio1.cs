d00352331418c07ac26c723d3a70301359a6e91a
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-01 21:20:30 -0400

    Ismael

--- Programll/EjerciciosCiclos/Ejercicio1.cs
+++ Programll/EjerciciosCiclos/Ejercicio1.cs
@@ -6,5 +6,15 @@ namespace Programll.EjerciciosCiclos
 {
     internal class Ejercicio1
     {
+        public void Main(string[] args)
+        {
+            int contador = 0;
+
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+        }
     }
 }
