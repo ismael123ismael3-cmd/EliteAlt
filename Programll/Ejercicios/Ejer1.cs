44094fd1ae01855ed26e3546d305064f65d349ef
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-01 21:30:13 -0400

    int

---
+++ Programll/Ejercicios/Ejer1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programll.Ejercicios
+{
+    public class Ejer1
+    {
+        static void Main(string[] args)
+
+        {
+            int vidas = 0;
+
+            while (vidas > 0)     // 0 > 0 es FALSO desde el inicio
+            {
+                Console.WriteLine("Te queda una vida");   // nunca se imprime
+                vidas--;
+            }
+    }
+}
